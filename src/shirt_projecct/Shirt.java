3e82c7fca0b0eaee4a9930120a8d439ca5f68379
Pratima Gangwar <pratimagangwar372@gmail.com>
2024-12-29 02:18:37 +0530

    Add files via upload

---
+++ src/shirt_projecct/Shirt.java
@@ -0,0 +1,21 @@
+package shirt_project;
+
+public class Shirt {
+	String brand;
+	int size;
+	double price;
+	String color;
+	Shirt(){
+		
+	}
+	Shirt(String brand, int size, double price, String color){
+		this.brand=brand;
+		this.size=size;
+		this.price=price;
+		this.color=color;
+	}
+	public String toString() {
+	return "Brand is: "+brand+"\tSize is: "+size+"\tPrice is: "+price+"\tColor is: "+color;
+	}
+
+}
